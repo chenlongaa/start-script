20ac978d67ff2cfe993f0cff11cb060c968eed68
chenlong <1031840348@qq.com>
2020-01-15 11:42:59 +0800

    config

---
+++ start-script-from-git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo snap install shadowsocks-libev
+git clone https://chenlongabc:neteqe=8656@github.com/chenlongabc/start-script.git
+sudo systemctl start shadowsocks-libev
--- start-script.sh
+++ start-script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 sudo snap install shadowsocks-libev
+echo "{
+    "server":"0.0.0.0",
+    "server_port":23333,
+    "local_address": "127.0.0.1",
+    "local_port":1080,
+    "password":"mypassword",
+    "timeout":300,
+    "method":"aes-256-cfb",
+    "fast_open": false
+}" > /etc/shadowsocks-libev/config.json
 sudo systemctl start shadowsocks-libev
