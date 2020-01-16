09d90e32c9e7ece58907f30d750ab42816ad4113
chenlong <1031840348@qq.com>
2020-01-16 11:25:03 +0800

    更改安装方式

--- start-script-from-git.sh
+++ start-script-from-git.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo snap install shadowsocks-libev
+sudo apt update
+sudo apt install shadowsocks-libev
 git clone https://chenlongabc:neteqe=8656@github.com/chenlongabc/start-script.git
+#mkdir /etc/shadowsocks-libev
+cp start-script/config.json /etc/shadowsocks-libev
 sudo systemctl start shadowsocks-libev
--- start-script2.sh
+++ start-script2.sh
@@ -5,12 +5,12 @@ sudo apt install shadowsocks-libev
 sudo -s
 echo "{
     "server":"0.0.0.0",
-    "server_port":23333,
+    "server_port":51105,
     "local_address": "127.0.0.1",
     "local_port":1080,
     "password":"mypassword",
     "timeout":300,
-    "method":"aes-256-cfb",
+    "method":"aes-256-gcm",
     "fast_open": false
 }" > /etc/shadowsocks-libev/config.json
 sudo systemctl start shadowsocks-libev
