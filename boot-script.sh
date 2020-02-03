15c0c2e7230b0b46aee831bb99080b3da55b977b
chenlong <1031840348@qq.com>
2020-02-03 20:58:50 +0800

    增加新的脚本

---
+++ boot-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#启动时运行的脚本
+git clone https://github.com/chenlongabc/start-script.git
+chmod +x ./start-script/proxy-install.sh
+./start-script/proxy-install.sh
---
+++ proxy-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#安装代理程序使用的脚本
+echo test > test.txt
+sudo apt update
+sudo apt install shadowsocks-libev
+#mkdir /etc/shadowsocks-libev
+sudo -s
+echo "{
+    "server":"0.0.0.0",
+    "server_port":51105,
+    "local_address": "127.0.0.1",
+    "local_port":1080,
+    "password":"mypassword",
+    "timeout":300,
+    "method":"aes-256-cfb",
+    "fast_open": false
+}" > /etc/shadowsocks-libev/config.json
+sudo systemctl start shadowsocks-libev
