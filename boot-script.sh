af6cf13f095bd4f6a2e232d30d8fbf0141b11b0d
chenlong <1031840348@qq.com>
2020-02-05 17:14:43 +0800

    取消cp 的-f

--- boot-script.sh
+++ boot-script.sh
@@ -2,4 +2,4 @@
 #启动时运行的脚本
 git clone https://github.com/chenlongabc/start-script.git
 chmod +x ./start-script/proxy-install.sh
-./start-script/proxy-install.sh
+source ./start-script/proxy-install.sh
--- proxy-install.sh
+++ proxy-install.sh
@@ -5,5 +5,5 @@ sudo apt update
 sudo apt -y install shadowsocks-libev
 #mkdir /etc/shadowsocks-libev
 sudo -s
-cp -f start-script/config.json /etc/shadowsocks-libev
+cp start-script/config.json /etc/shadowsocks-libev
 sudo systemctl start shadowsocks-libev
