b831ac2670b59f941bb8e0e2822a53ae481f0256
chenlong <1031840348@qq.com>
2020-02-05 17:42:04 +0800

    删除sudo -s

--- proxy-install.sh
+++ proxy-install.sh
@@ -4,6 +4,5 @@ echo test > test.txt
 sudo apt update
 sudo apt -y install shadowsocks-libev
 #mkdir /etc/shadowsocks-libev
-sudo -s
-cp start-script/config.json /etc/shadowsocks-libev
+sudo cp start-script/config.json /etc/shadowsocks-libev
 sudo systemctl start shadowsocks-libev
