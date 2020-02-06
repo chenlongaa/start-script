15a5a69a822f84e01a89ec004b52d3a2f578cbb8
chenlong <1031840348@qq.com>
2020-02-06 21:02:12 +0800

    增加bbr配置

--- proxy-install.sh
+++ proxy-install.sh
@@ -15,3 +15,17 @@ wget https://install.direct/go.sh
 bash go.sh
 cp start-script/v2ray/config.json /etc/v2ray/config.json
 systemctl start v2ray
+
+#配置bbr
+#开启TCP BBR
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+#保存生效，配置内核
+sysctl   -p
+#查看内核是否已开启BBR
+#看bbr是否处于available列表中
+sysctl net.ipv4.tcp_available_congestion_control
+#查看bbr是否是被配置用于tcp congestion control
+sysctl net.ipv4.tcp_congestion_control
+#查看bbr是否已经启动
+lsmod | grep bbr
